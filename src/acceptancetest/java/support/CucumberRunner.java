b835e840b1ea4101136b5de05f1403e2fc96c1c8
pradeepk6 <pradeepk6@outlook.com>
2019-05-08 16:06:34 +0100

    added travis build file

--- src/acceptancetest/java/support/CucumberRunner.java
+++ src/acceptancetest/java/support/CucumberRunner.java
@@ -2,10 +2,7 @@ package support;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
-import utils.TestDataUtils;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -15,26 +12,6 @@ import utils.TestDataUtils;
         monochrome = true,
         plugin = {"html:build/cucumber-reports"})
 public class CucumberRunner {
-
-    /**
-     * gets test data and appends to feature file DataTable
-     */
-    /*
-    @BeforeClass
-    public static void testDataSetUp(){
-       // TestDataUtils.testDataSetUp();
-    }
-     */
-
-    /**
-     * remvoves test data from feature file DataTable
-     */
-    /*
-    @AfterClass
-    public static void testDataTearDown(){
-       // TestDataUtils.testDataTearDown();
-    }
-     */
 }
 
 
