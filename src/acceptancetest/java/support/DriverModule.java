a65b91a82e5e1ff6afa46dd70af67ee363e12ba6
pradeepk6 <pradeepk6@outlook.com>
2019-05-14 08:14:11 +0100

    designed for driver reuse

---
+++ src/acceptancetest/java/support/DriverModule.java
@@ -0,0 +1,31 @@
+package support;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Scopes;
+import org.openqa.selenium.WebDriver;
+
+public class DriverModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+        String browser = System.getProperty("browser");
+        switch (browser) {
+            case "CHROME":
+                bind(DriverManager.class).to(ChromeDriverManager.class).in(Scopes.SINGLETON);
+                break;
+            case "FIREFOX":
+                //todo
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown browser name : " + browser);
+        }
+    }
+
+    @Provides
+    public WebDriver getDriver(DriverManager driverManager) {
+        return driverManager.getDriver();
+    }
+
+}
